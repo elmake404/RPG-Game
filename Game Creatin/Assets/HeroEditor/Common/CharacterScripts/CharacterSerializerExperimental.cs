be0270e823b184ddd514a92765ab7edd18c52d25
elmake <62005075+elmake404@users.noreply.github.com>
2020-04-06 14:44:21 +0300

    Анимация Рыцаря

---
+++ Game Creatin/Assets/HeroEditor/Common/CharacterScripts/CharacterSerializerExperimental.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HeroEditor.Common;
+using HeroEditor.Common.Data;
+using HeroEditor.Common.Enums;
+using UnityEngine;
+
+namespace Assets.HeroEditor.Common.CharacterScripts
+{
+	public partial class Character
+	{
+		public override string ToJson()
+		{
+			var sc = SpriteCollection.Instance;
+
+			if (sc == null) throw new Exception("SpriteCollection is missed on scene!");
+
+			var description = new SerializableDictionary<string, string>
+			{
+				{ "Head", GetSpriteEntryId(sc.Head, Head) },
+				{ "Body", GetSpriteEntryId(sc.Body, Body) },
+				{ "Ears", GetSpriteEntryId(sc.Ears, Ears) },
+				{ "Hair", GetSpriteEntryId(sc.Hair, Hair) },
+				{ "Beard", GetSpriteEntryId(sc.Beard, Beard) },
+				{ "Helmet", GetSpriteEntryId(sc.Helmet, Helmet) },
+				{ "Glasses", GetSpriteEntryId(sc.Glasses, Glasses) },
+				{ "Mask", GetSpriteEntryId(sc.Mask, Mask) },
+				{ "Armor", GetSpriteEntryId(sc.Armor, Armor) },
+				{ "PrimaryMeleeWeapon", GetSpriteEntryId(GetWeaponCollection(WeaponType), PrimaryMeleeWeapon) },
+				{ "SecondaryMeleeWeapon", GetSpriteEntryId(GetWeaponCollection(WeaponType), SecondaryMeleeWeapon) },
+				{ "Cape", GetSpriteEntryId(sc.Cape, Cape) },
+				{ "Back", GetSpriteEntryId(sc.Back, Back) },
+				{ "Shield", GetSpriteEntryId(sc.Shield, Shield) },
+				{ "Bow", GetSpriteEntryId(sc.Bow, Bow) },
+				{ "Firearms", GetSpriteEntryId(GetWeaponCollection(WeaponType), Firearms) },
+				{ "FirearmParams", Firearm.Params.Name },
+				{ "WeaponType", WeaponType.ToString() },
+				{ "Expression", Expression }
+			};
+
+			foreach (var expression in Expressions)
+			{
+				description.Add(string.Format("Expression.{0}.Eyebrows", expression.Name), GetSpriteEntryId(sc.Eyebrows, expression.Eyebrows));
+				description.Add(string.Format("Expression.{0}.Eyes", expression.Name), GetSpriteEntryId(sc.Eyes, expression.Eyes));
+				description.Add(string.Format("Expression.{0}.Mouth", expression.Name), GetSpriteEntryId(sc.Mouth, expression.Mouth));
+			}
+
+			return JsonUtility.ToJson(description);
+		}
+
+		public override void LoadFromJson(string serialized)
+		{
+		}
+
+		private static IEnumerable<SpriteGroupEntry> GetWeaponCollection(WeaponType weaponType)
+		{
+			switch (weaponType)
+			{
+				case WeaponType.Melee1H: return SpriteCollection.Instance.MeleeWeapon1H;
+				case WeaponType.MeleePaired: return SpriteCollection.Instance.MeleeWeapon1H;
+				case WeaponType.Melee2H: return SpriteCollection.Instance.MeleeWeapon2H;
+				case WeaponType.Bow: return SpriteCollection.Instance.Bow;
+				case WeaponType.Firearms1H: return SpriteCollection.Instance.Firearms1H;
+				case WeaponType.FirearmsPaired: return SpriteCollection.Instance.Firearms1H;
+				case WeaponType.Firearms2H: return SpriteCollection.Instance.Firearms2H;
+				case WeaponType.Supplies: return SpriteCollection.Instance.Supplies;
+				default:
+					throw new NotSupportedException(weaponType.ToString());
+			}
+		}
+
+		private static string GetSpriteEntryId(IEnumerable<SpriteGroupEntry> collection, Sprite sprite)
+		{
+			if (sprite == null) return null;
+
+			var entry = collection.SingleOrDefault(i => i.Sprite == sprite);
+
+			if (entry == null) throw new Exception(string.Format("Can't find {0} in SpriteCollection.", sprite.name));
+			
+			return entry.Id;
+		}
+
+		private static string GetSpriteEntryId(IEnumerable<SpriteGroupEntry> collection, List<Sprite> sprites)
+		{
+			if (sprites == null || sprites.Count == 0) return null;
+
+			return GetSpriteEntryId(collection, sprites[0]);
+		}
+
+		private static Sprite FindSpriteById(IEnumerable<SpriteGroupEntry> collection, string id)
+		{
+			if (string.IsNullOrEmpty(id)) return null;
+
+			var entries = collection.Where(i => i.Id == id).ToList();
+
+			switch (entries.Count)
+			{
+				case 1:
+					return entries[0].Sprite;
+				case 0:
+					throw new Exception(string.Format("Entry with id {0} not found in SpriteCollection.", id));
+				default:
+					throw new Exception(string.Format("Multiple entries with id {0} found in SpriteCollection.", id));
+			}
+		}
+
+		private static List<Sprite> FindSpritesById(IEnumerable<SpriteGroupEntry> collection, string id)
+		{
+			if (string.IsNullOrEmpty(id)) return new List<Sprite>();
+
+			var entries = collection.Where(i => i.Id == id).ToList();
+
+			switch (entries.Count)
+			{
+				case 1:
+					return entries[0].Sprites;
+				case 0:
+					throw new Exception(string.Format("Entry with id {0} not found in SpriteCollection.", id));
+				default:
+					throw new Exception(string.Format("Multiple entries with id {0} found in SpriteCollection.", id));
+			}
+		}
+	}
+}
---
+++ Game Creatin/Assets/HeroEditor/Common/EditorScripts/CharacterEditor.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Linq;
+using Assets.HeroEditor.Common.CharacterScripts;
+using Assets.HeroEditor.Common.ExampleScripts;
+using HeroEditor.Common;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.HeroEditor.Common.EditorScripts
+{
+    /// <summary>
+    /// Character editor UI and behaviour.
+    /// </summary>
+    public class CharacterEditor : CharacterEditorBase
+    {
+        [Header("Other")]
+        public FirearmCollection FirearmCollection;
+        public bool UseEditorColorField = true;
+        public string PrefabFolder;
+        public string TestRoomSceneName;
+	    
+        private static Character _temp;
+
+        /// <summary>
+        /// Called automatically on app start.
+        /// </summary>
+        public void Awake()
+        {
+            RestoreTempCharacter();
+        }
+
+		/// <summary>
+        /// Remove all equipment.
+        /// </summary>
+        public void Reset()
+        {
+            ((Character) Character).ResetEquipment();
+            InitializeDropdowns();
+        }
+
+        #if UNITY_EDITOR
+
+        /// <summary>
+        /// Save character to prefab.
+        /// </summary>
+        public void Save()
+        {
+            PrefabFolder = UnityEditor.EditorUtility.SaveFilePanel("Save character prefab", PrefabFolder, "New character", "prefab");
+
+	        if (PrefabFolder.Length > 0)
+	        {
+		        Save("Assets" + PrefabFolder.Replace(Application.dataPath, null));
+	        }
+		}
+
+	    /// <summary>
+		/// Load character from prefab.
+		/// </summary>
+		public void Load()
+        {
+	        FeatureTip();
+		}
+
+	    /// <summary>
+	    /// Save character to json.
+	    /// </summary>
+	    public void SaveToJson()
+	    {
+			PrefabFolder = UnityEditor.EditorUtility.SaveFilePanel("Save character to json", PrefabFolder, "New character", "json");
+
+		    if (PrefabFolder.Length > 0)
+		    {
+			    var path = "Assets" + PrefabFolder.Replace(Application.dataPath, null);
+			    var json = Character.ToJson();
+
+			    System.IO.File.WriteAllText(path, json);
+			    Debug.LogFormat("Json saved to {0}: {1}", path, json);
+		    }
+
+		    //FeatureTip();
+		}
+
+		/// <summary>
+		/// Load character from json.
+		/// </summary>
+		public void LoadFromJson()
+	    {
+			FeatureTip();
+	    }
+
+		public override void Save(string path)
+		{
+			Character.transform.localScale = Vector3.one;
+
+			#if UNITY_2018_3_OR_NEWER
+
+			UnityEditor.PrefabUtility.SaveAsPrefabAsset(Character.gameObject, path);
+
+			#else
+
+			UnityEditor.PrefabUtility.CreatePrefab(path, Character.gameObject);
+
+			#endif
+
+            Debug.LogFormat("Prefab saved as {0}", path);
+        }
+
+        public override void Load(string path)
+        {
+			var character = UnityEditor.AssetDatabase.LoadAssetAtPath<Character>(path);
+
+	        Character.GetComponent<Character>().Firearm.Params = character.Firearm.Params; // TODO: Workaround
+			Load(character);
+			FindObjectOfType<CharacterBodySculptor>().OnCharacterLoaded(character);
+        }
+
+	    #else
+
+        public override void Save(string path)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void Load(string path)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endif
+
+        /// <summary>
+        /// Test character with demo setup.
+        /// </summary>
+        public void Test()
+        {
+            FeatureTip();
+		}
+
+		protected override void SetFirearmParams(SpriteGroupEntry entry)
+        {
+            if (entry == null) return;
+
+            if (FirearmCollection.Firearms.Count(i => i.Name == entry.Name) != 1) throw new Exception("Please check firearms params for: " + entry.Name);
+
+			((Character) Character).Firearm.Params = FirearmCollection.Firearms.Single(i => i.Name == entry.Name);
+		}
+
+	    protected override void OpenPalette(GameObject palette, Color selected)
+        {
+            #if UNITY_EDITOR_WIN
+
+            if (UseEditorColorField)
+            {
+                EditorColorField.Open(selected);
+            }
+            else
+
+            #endif
+
+            {
+                Editor.SetActive(false);
+                palette.SetActive(true);
+            }
+        }
+
+        private void RestoreTempCharacter()
+        {
+            if (_temp == null) return;
+
+	        Character.GetComponent<Character>().Firearm.Params = _temp.Firearm.Params; // TODO: Workaround
+			Load(_temp);
+	        FindObjectOfType<CharacterBodySculptor>().OnCharacterLoaded(_temp);
+			Destroy(_temp.gameObject);
+            _temp = null;
+        }
+
+	    protected override void FeedbackTip()
+	    {
+			#if UNITY_EDITOR
+
+		    var success = UnityEditor.EditorUtility.DisplayDialog("Hero Editor", "Hi! Thank you for using my asset! I hope you enjoy making your game with it. The only thing I would ask you to do is to leave a review on the Asset Store. It would be awasome support for my asset, thanks!", "Review", "Later");
+			
+			RequestFeedbackResult(success, true);
+
+			#endif
+	    }
+
+	    private void FeatureTip()
+	    {
+			#if UNITY_EDITOR
+
+		    if (UnityEditor.EditorUtility.DisplayDialog("Hero Editor", "This feature is available only in PRO asset version!", "Navigate", "Cancel"))
+		    {
+			    Application.OpenURL(LinkToProVersion);
+		    }
+
+			#endif
+		}
+    }
+}
